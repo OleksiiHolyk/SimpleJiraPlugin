5537c40a042bb57b4911b59a1a2a0fc19522c650
Oleksii Holyk <oleksii.holyk@outlook.com>
2017-12-18 17:04:34 +0200

    - in servlet response we can find all of our issues by authorized user  (we create demo project using web IU inside jira web)

--- src/main/java/ua/oleksiiholyk/servlet/MainServlet.java
+++ src/main/java/ua/oleksiiholyk/servlet/MainServlet.java
@@ -1,5 +1,13 @@
 package ua.oleksiiholyk.servlet;
 
+import com.atlassian.jira.bc.issue.search.SearchService;
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.search.SearchException;
+import com.atlassian.jira.jql.builder.JqlClauseBuilder;
+import com.atlassian.jira.jql.builder.JqlQueryBuilder;
+import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.jira.web.bean.PagerFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,15 +16,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-public class MainServlet extends HttpServlet{
+public class MainServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(MainServlet.class);
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-    {
-        resp.setContentType("text/html");
-        resp.getWriter().write("<html><body>Hello World</body></html>");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        List<Issue> issues = getIssues(getCurrentUser());
+        resp.setContentType("text/html;charset=utf-8");
+        resp.getWriter().write("<html><body>" + issues + "</body></html>");
+    }
+
+    private List<Issue> getIssues(ApplicationUser applicationUser) {
+        PagerFilter pagerFilter = PagerFilter.getUnlimitedFilter();
+        JqlClauseBuilder jqlClauseBuilder = JqlQueryBuilder.newClauseBuilder();
+        SearchService searchService = ComponentAccessor
+                .getComponentOfType(SearchService.class);
+        com.atlassian.query.Query query = jqlClauseBuilder.project("TUTORIAL").buildQuery();
+        com.atlassian.jira.issue.search.SearchResults searchResults = null;
+        try {
+            searchResults = searchService.search(applicationUser, query, pagerFilter);
+        } catch (SearchException e) {
+            e.printStackTrace();
+        }
+        return searchResults.getIssues();
+    }
+
+    private ApplicationUser getCurrentUser() {
+        return ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser();
     }
 
 }

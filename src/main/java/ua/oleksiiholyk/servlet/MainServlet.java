1011b1e192179e0f57d9ad175c5cedb172ed2500
Oleksii <oleksii.holyk@outlook.com>
2017-12-18 22:17:36 +0200

    - add velocity as template - data goes from doGet servlet method packed to HashMap for view form (with velocity)

--- src/main/java/ua/oleksiiholyk/servlet/MainServlet.java
+++ src/main/java/ua/oleksiiholyk/servlet/MainServlet.java
@@ -8,6 +8,8 @@ import com.atlassian.jira.jql.builder.JqlClauseBuilder;
 import com.atlassian.jira.jql.builder.JqlQueryBuilder;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.web.bean.PagerFilter;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.templaterenderer.TemplateRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,17 +18,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(MainServlet.class);
+
+    @ComponentImport
+    private final TemplateRenderer renderer;
+
+    private static final String TABLE_TEMPLATE = "/templates/table.vm";
+
+    public MainServlet(TemplateRenderer renderer) {
+        this.renderer = renderer;
+    }
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         List<Issue> issues = getIssues(getCurrentUser());
+        Map<String, Object> context = new HashMap<String, Object>();
+        context.put("issues", issues);
+
         resp.setContentType("text/html;charset=utf-8");
-        resp.getWriter().write("<html><body>" + issues + "</body></html>");
+        renderer.render(TABLE_TEMPLATE, context, resp.getWriter());
     }
 
     private List<Issue> getIssues(ApplicationUser applicationUser) {
